3d9e251f825c240b45d8b100fa80b16ec688a8df
Evgenii Safronov <emsafronov2004@gmail.com>
2022-10-13 16:24:44 +0300

    4 r

--- cp/3/3.ind.cpp
+++ cp/3/3.ind.cpp
@@ -1,3 +1,4 @@
+#include <ctime>
 #include <iostream>
 using namespace std;
 int main() {
@@ -5,17 +6,19 @@ int main() {
   int maxX = 0;
   int maxY = 0;
   int maxA = 0;
+  srand(time(NULL));
   cout << "Введите порядок квадратной матрицы n > ";
   cin >> n;
-
+  cout << "----------------------" << endl;
   auto **arr = new int *[n];
-cout << "Введите матрицу"<<endl;
+  cout << "Исходная матрица" << endl;
   for (int i = 0; i < n; i++) {
     arr[i] = new int[n];
     for (int j = 0; j < n; j++) {
-      cin >> arr[i][j];
+      arr[i][j] = rand() % 2000 - 1000;
+      cout << arr[i][j] << " ";
     }
-  }
+    cout << endl;}
   for (int i = 0; i < n; i++) {
     if (arr[i][i] > maxA) {
       maxA = arr[i][i];
@@ -24,17 +27,17 @@ cout << "Введите матрицу"<<endl;
     if (arr[n - i - 1][i] > maxA) {
       maxA = arr[n - i - 1][i];
       maxY = i;
-      maxX = n - i - 1;
-    }
-  }
+      maxX = n - i - 1;}}
   swap(arr[maxX][maxY], arr[(n - 1) / 2][(n - 1) / 2]);
-cout << "----------------------"<< endl;
-cout << "Отсортированная матрица"<<endl;
+  cout << "----------------------" << endl;
+  cout << "Отсортированная матрица" << endl;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       cout << arr[i][j] << ' ';
     }
     cout << endl;
   }
+  for (int i = 0; i < n; i++) delete[] arr[i];
+  delete[] arr;
   return 0;
 }
---
+++ cp/4/4.1.cpp
@@ -0,0 +1,13 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  unsigned char ch;
+//   cin >> ch;
+//   if 
+  cout << "#\n# Строчно-заглавное преобразование\n#\n\n";
+  return 0;
+}
